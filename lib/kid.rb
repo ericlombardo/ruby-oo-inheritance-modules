f76dbae1bd3e6ff4d2b924fcffb893b01e328675
Eric <eric.m.lombardo@gmail.com>
2020-12-07 18:01:57 -0500

    commit tree ccf32149b0b8f881b57eab3ea09951b28c3c410a

--- lib/kid.rb
+++ lib/kid.rb
@@ -1,2 +1,15 @@
+require_relative './class_methods_module.rb' 
 require_relative './dance_module.rb'
-require_relative './class_methods_module.rb'
+require_relative './fancy_dance.rb'   # include files to have access
+
+class Kid
+  include Dance # imports Dance module for instance methods
+  extend MetaDancing  # imports MetaDancing for class methods
+  include FancyDance::InstanceMethods # imports InstanceMethods module that is in FancyDance module for instance methods
+  extend FancyDance::ClassMethods     # imports ClassMethods module that is in FancyDance module for class methods
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+end
